85fb5c65966eb2f73e8c8e060dc2724d12443c27
Friederich Nietzsche <frnitzsche@yahoo.ca>
2025-07-14 15:08:46 +0300

    aa

--- customization-script.sh
+++ customization-script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 host="mongo-anishev.mywire.org"
-sudo yum install docker -y && \
+sudo yum install docker nginx certbot certbot-nginx httpd-tools -y && \
+sudo htpasswd -b -c /etc/nginx/.htpasswd user '478312zxc'
 sudo systemctl enable docker.service && \
 sudo systemctl start docker.service && \
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-linux-$(uname -m) -o /usr/bin/docker-compose && \
@@ -12,11 +13,12 @@ sudo yum install ./dynu-ip-update-client_1.0.2-1_amd64.rpm -y && \
 sudo cp /docker-compose-mongo/appsettings.json /usr/share/dynu-ip-update-client/appsettings.json && \
 sudo systemctl restart dynu-ip-update-client.service && \
 sleep 5s && \
-sudo yum install nginx certbot certbot-nginx -y && \
 sudo sed -i "s/server_name  _;/server_name ${host};/g" /etc/nginx/nginx.conf && \
 
 sed '/root         \/usr\/share\/nginx\/html;/r'<(cat <<EOF
         location / {
+            auth_basic           "Restricted Access Area";
+            auth_basic_user_file /etc/nginx/.htpasswd;
             proxy_pass http://127.0.0.1:8082;
             proxy_set_header Host \$host;
             proxy_set_header X-Real-IP \$remote_addr;
