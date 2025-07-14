e33590574c495ce675c8ab342353c9529e61234f
Friederich Nietzsche <frnitzsche@yahoo.ca>
2025-07-14 13:47:20 +0300

    bugfix

--- aws-ec2-instance-user-data.sh
+++ aws-ec2-instance-user-data.sh
@@ -2,6 +2,6 @@
 sudo yum update -y && \
 sudo yum install git -y && \
 git clone https://github.com/frnitzsche/docker-compose-mongo.git && \
-cd docker-compose-beo && \
+cd docker-compose-mongo && \
 chmod a+x customization-script.sh && \
 /bin/bash customization-script.sh
